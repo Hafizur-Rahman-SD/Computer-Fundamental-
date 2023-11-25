4b01874239aa19cb3672c65cbbbb20116a6a9890
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-11-25 13:15:30 +0600

    Update computer_basics.c

--- computer_basics.c
+++ computer_basics.c
@@ -1,2 +1,5 @@
 
 here i will write basics part of computer. 
+question: What is computer?
+answer:  computer is an electronic device that manipulates information, or data. 
+          It has the ability to store, retrieve, and process data.
